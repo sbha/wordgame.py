6e6909b9e3d21e725aac84d6daa3117e3fdbfc33
“sbha” <sbharty@gmail.com>
2019-02-02 15:28:08 -0600

    first commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+========
+wordgame
+========
+
+Overview
+========
+Find words four letters or more from a group of seven letters.
+
+Usage
+=====
+    from wordgame import wordgame as wg
+
+Start the game:
+    wg.start_game()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+LONG_DESCRIPTION = """
+Find words four letters or more from a group of seven letters
+
+"""
+
+setup(
+    name='wordgame',
+    version='0.0.1',
+    description='A word game',
+    long_description=LONG_DESCRIPTION,
+    license='MIT',
+    packages=['wordgame'],
+    install_requires=[
+          'pandas',
+      ]
+    author='A true artist',
+    author_email='stuart.harty@email.com',
+    keywords=['games', 'vocabulary'],
+    url='https://github.com/sbha/wordgame.py'
+)
+
+
